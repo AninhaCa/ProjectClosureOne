6372d95207ec606a94f2fd650ca0b2e1e8edd01f
Aninha <aninhapss2000@gmail.com>
2023-03-22 20:48:35 -0300

    Added PriceViewController 2 button, 4 label e 1 image

--- ProjectClosureOne/ProjectClosureOne/ViewController.swift
+++ ProjectClosureOne/ProjectClosureOne/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var arrayPizza: Pizza?
+    
+    let request = RequestPizza()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
+    
+    func setupTableView() {
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MyCustomCellXIB", bundle: nil), forCellReuseIdentifier: "cellXIB")
+        request.requestPizza { pizza in
+            self.arrayPizza = pizza
+            self.tableView.reloadData()
+        }
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayPizza?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cellXIB", for: indexPath) as? MyCustomCellXIB {
+            cell.setupXIB(pizza: arrayPizza?[indexPath.row])
+            
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
 }
 

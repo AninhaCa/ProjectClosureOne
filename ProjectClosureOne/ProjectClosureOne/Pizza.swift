6827fb8f2b256e4e57c70b3183276dcb976c1b5c
Aninha <aninhapss2000@gmail.com>
2023-03-22 20:15:02 -0300

    Added ProjectClosureOne, Classe Pizza

---
+++ ProjectClosureOne/ProjectClosureOne/Pizza.swift
@@ -0,0 +1,8 @@
+//
+//  Pizza.swift
+//  ProjectClosureOne
+//
+//  Created by Ana Paula Silva de Sousa on 22/03/23.
+//
+
+import Foundation
---
+++ ProjectClosureOne/ProjectClosureOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectClosureOne
+//
+//  Created by Ana Paula Silva de Sousa on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
